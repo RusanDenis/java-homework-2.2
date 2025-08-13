9d9599772892be1a8cd610e81c1a1e667c5d858a
RusanDenis <rusandenis18@gmail.com>
2025-08-13 20:26:30 +0300

    bonus mobile

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //если клиент пополняет счёт более чем на 1000 рублей,
+        //то сотовый оператор дарит ему по 1 рублю за каждые полные 100 рублей пополнения.
+
+        int first = 100;
+        int second = 1100;
+        int bonus =0;
+        boolean isEnter = second>1000;
+        if(isEnter)
+            bonus= first+second+second/100;
+        else
+            bonus=first+second;
+
+        System.out.println(bonus);
+    }
+}
